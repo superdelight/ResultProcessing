c3b0f2cefe1f5c3f59e2fa832480e5182ddc1071
Superdelight <delighttechnosoft@gmail.com>
2017-08-31 23:43:35 +0100

    [Tolulope Awode] DAL_Update_2: Course Context was created

---
+++ RsManager_Version2/DAL/Repository/Implementation/CourseRepository.cs
@@ -0,0 +1,58 @@
+﻿using DAL.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repository.Implementation
+{
+    public class CourseRepository : Repository<Course>, ICourseRepository
+    {
+        private DbContext Context;
+        public CourseRepository(DbContext Context)
+            : base(Context)
+        {
+            this.Context = Context;
+        }
+
+        public bool ConfirmCourse(string courseTitle, string code)
+        {
+            return Context.Set<Course>().Where(c => c.CourseTitle.ToLower().Equals(courseTitle.ToLower()) ||
+            c.CourseCode.ToLower().Equals(code.ToLower())).Any();
+        }
+
+   
+        public IEnumerable<Course> GetAllCourses(int deptId)
+        {
+            return Context.Set<Course>().Where(d => d.DeptId.Equals(deptId));
+        }
+
+     
+
+        public Course GetCourseByCode(string code)
+        {
+            return Context.Set<Course>().Where(c => c.CourseCode.ToLower().Equals(code.ToLower())).FirstOrDefault();
+        }
+
+        public Course GetCourseByTitle(string title)
+        {
+            return Context.Set<Course>().Where(c => c.CourseTitle.ToLower().Equals(title.ToLower())).FirstOrDefault();
+        }
+
+        public int UploadCourses(IEnumerable<Course> Courses)
+        {
+            int count = 0;
+            foreach(var c in Courses)
+            {
+                if (ConfirmCourse(c.CourseTitle, c.CourseCode) == false)
+                {
+                    Context.Set<Course>().Add(c);
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ RsManager_Version2/DAL/Repository/Interface/ICourseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repository.Interface
+{
+    public interface ICourseRepository : IRepository<Course>
+    {
+        int UploadCourses(IEnumerable<Course> Courses);
+        Course GetCourseByTitle(string title);
+        Course GetCourseByCode(string code);
+        bool ConfirmCourse(string courseTitle, string code);
+        IEnumerable<Course> GetAllCourses(int deptId);
+    }
+}
